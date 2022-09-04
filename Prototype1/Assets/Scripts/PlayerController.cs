b8de348c704ab8239cdb03c447962c820a2c0d91
AdamField7 <89855015+AdamField7@users.noreply.github.com>
2022-09-04 16:04:09 -0500

    added prototype1 gameplay

---
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float horizontalInput;
+    public float forwardInput;
+    public float turnSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //move forward with forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //rotate player with horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
