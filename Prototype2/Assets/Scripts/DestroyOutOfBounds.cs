c1c17ddf4ca3a7e9f672ab4ea65c2a7fd0fb1b88
AdamField7 <89855015+AdamField7@users.noreply.github.com>
2022-09-06 16:02:36 -0500

    completed prototype 2

---
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottomBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if banana goes out of bounds
+        if(transform.position.z > topBound)
+        {
+            //destroy banana
+            Destroy(gameObject);
+        }
+        //if animals go out of bounds
+        if(transform.position.z < bottomBound)
+        {
+            //get health system script and call take damage
+            GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>().TakeDamage();
+
+            //destroy animal
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //drag the animals to spawn into this array
+    public GameObject[] animalsToSpawn;
+
+    private float leftBound = -14;
+    private float rightBound = 14;
+    private float spawnPosZ = 20;
+
+    public HealthSystem healthSystem;
+
+    private void Start()
+    {
+        healthSystem = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>(); 
+
+        StartCoroutine(SpawnRandomPrefabCoroutine());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomPrefab();
+        }
+    }
+
+    IEnumerator SpawnRandomPrefabCoroutine()
+    {
+        //3 second delay
+        yield return new WaitForSeconds(3f);
+
+        while(!healthSystem.gameOver)
+        {
+            SpawnRandomPrefab();
+
+            float randomDelay = Random.Range(0.8f, 2.0f);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    void SpawnRandomPrefab()
+    {
+        //generate index
+        int prefabIndex = Random.Range(0, animalsToSpawn.Length);
+
+        //generate spawn pos
+        Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+        //spawn
+        Instantiate(animalsToSpawn[prefabIndex], spawnPos, animalsToSpawn[prefabIndex].transform.rotation);
+    }
+}
