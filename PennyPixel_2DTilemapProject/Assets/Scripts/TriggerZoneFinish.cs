653401bc331b08e593e98eea41ea7b02e63a05d9
AdamField7 <89855015+AdamField7@users.noreply.github.com>
2022-10-04 16:27:35 -0500

    Assignment 5A fully completed

---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/TriggerZoneFinish.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Adam Field
+ * Assignment 5A
+ * makes the trigger zone work
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneFinish : MonoBehaviour
+{
+    private UIManager uIManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uIManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("finish");
+            uIManager.YouWin();
+        }
+    }
+
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Adam Field
+ * Assignment 5A
+ * controls UI for the game
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    public GameObject winText;
+
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+
+        scoreText.text = "Gems: 0/10";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Gems: " + score + "/10";
+    }
+
+    public void YouWin()
+    {
+        winText.SetActive(true);
+    }
+}
