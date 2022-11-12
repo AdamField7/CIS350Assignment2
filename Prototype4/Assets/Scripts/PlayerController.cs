986d0c0f56d3008fb4ebb96f8f9811a89a34a402
AdamField7 <89855015+AdamField7@users.noreply.github.com>
2022-11-12 17:53:32 -0600

    Completed Prototype4 video and additonal requirements

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿/*
+* Adam Field
+* Prototype4
+* controls player movement
+*/
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed;
+    private float forwardInput;
+
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+
+    public GameObject powerupIndicator;
+
+    public UIManager UIManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("FocalPoint");
+        if (UIManagerScript == null)
+        {
+            UIManagerScript = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -.5f, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        if(transform.position.y < -10)
+        {
+            Time.timeScale = 0;
+            UIManagerScript.Lose();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Player Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position).normalized;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Prototype4/Assets/Scripts/UIManager.cs
@@ -0,0 +1,79 @@
+﻿/*
+* Adam Field
+* Prototype4
+* Manages the timescale of the game and the UI
+*/
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Text lostText;
+    public Text winText;
+    public Text startText;
+    public Text waveText;
+
+    public bool loseCondition = false;
+    public bool winCondition = false;
+
+
+    public SpawnManager spawnManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+
+        if (spawnManagerScript == null)
+        {
+            spawnManagerScript = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waveText.text = "Wave Number: " + spawnManagerScript.waveNumber;
+        if(startText.IsActive() == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                startText.gameObject.SetActive(false);
+                waveText.gameObject.SetActive(true);
+                Time.timeScale = 1;
+            }
+        }
+
+        if (loseCondition == true)
+        {
+            lostText.gameObject.SetActive(true);
+        }
+
+        if (winCondition == true)
+        {
+            winText.gameObject.SetActive(true);
+        }
+
+        if (Time.timeScale == 0 && Input.GetKeyDown(KeyCode.R) && startText.IsActive() == false)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void Win()
+    {
+        winCondition = true;
+    }
+
+    public void Lose()
+    {
+        loseCondition = true;
+    }
+}
