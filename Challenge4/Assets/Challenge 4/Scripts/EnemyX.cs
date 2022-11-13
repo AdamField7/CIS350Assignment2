08554e19edde51b8386241cd90cdfb73810c6d1a
AdamField7 <89855015+AdamField7@users.noreply.github.com>
2022-11-13 13:22:01 -0600

    Challenge 4 completed

---
+++ Challenge4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,65 @@
+﻿/*
+* Adam Field
+* Challenge4
+* Controls enemies
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX spawnManagerXScript;
+    private UIManager UIManagerScript;
+    public int speedModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnManagerXScript == null)
+        {
+            spawnManagerXScript = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManagerX>();
+        }
+        if (UIManagerScript == null)
+        {
+            UIManagerScript = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+        }
+
+        Debug.Log("wave count = " + spawnManagerXScript.waveCount);
+        speed = spawnManagerXScript.waveCount * speedModifier;
+        Debug.Log("speed = " + speed);
+
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.FindGameObjectWithTag("PlayerGoal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+            UIManagerScript.enemiesScored++;
+        }
+
+    }
+
+}
---
+++ Challenge4/Assets/Challenge 4/Scripts/UIManager.cs
@@ -0,0 +1,85 @@
+﻿/*
+* Adam Field
+* Challenge4
+* Manages the timescale of the game and the UI
+*/
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Text lostText;
+    public Text winText;
+    public Text startText;
+    public Text waveText;
+
+    public bool loseCondition = false;
+    public bool winCondition = false;
+
+    public int enemiesScored = 0;
+
+
+    public SpawnManagerX spawnManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+
+        if (spawnManagerScript == null)
+        {
+            spawnManagerScript = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManagerX>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waveText.text = "Wave Number: " + spawnManagerScript.waveCount;
+        if (startText.IsActive() == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                startText.gameObject.SetActive(false);
+                waveText.gameObject.SetActive(true);
+                Time.timeScale = 1;
+            }
+        }
+
+        if(spawnManagerScript.waveCount == 11)
+        {
+            Win();
+        }
+
+        if (enemiesScored >= spawnManagerScript.waveCount && spawnManagerScript.waveCount > 0)
+        {
+            Lose();
+        }
+
+        if (Time.timeScale == 0 && Input.GetKeyDown(KeyCode.R) && startText.IsActive() == false)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void Win()
+    {
+        winCondition = true;
+        winText.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Lose()
+    {
+        loseCondition = true;
+        lostText.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
